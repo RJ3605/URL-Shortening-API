93183e8aadaa2c31c780a50f9e9772b8297b5cd8
Ronald Sayre III <115036926+RJ3605@users.noreply.github.com>
2022-12-14 09:00:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
 /*import FetchWrapper from "./fetch-wrapper.js";*/
 
 const shortenedLinks = document.querySelector("#shortened-links");
+const shortenedLink = document.querySelector(".shortened-li");
 const shortenIt = document.querySelector("#shorten-it");
 const urlInput = document.querySelector("#url-input");
 const copy = document.querySelector(".copy");
 const copyButtons = document.querySelectorAll(".copy");
-const copiedLink = document.querySelector(".shortened-link");
-const shortenedLink = document.querySelector(".shortened-link");
+const output = document.querySelector(".output");
 /*const API = new FetchWrapper('')*/
 
 /*This places the html segment that contains the original and shortened links*/
-shortenIt.addEventListener("click", () => {
+shortenIt.addEventListener("click", async () => {
   if (urlInput.value !== "") {
+    /*submit posts to API
+    await API get link*/
     shortenedLinks.insertAdjacentHTML(
       `beforeend`,
-      `<div class="shortened-div">
-        <p class="original-link">${urlInput.value}</p>
-        <div>
-          <a class="shortened-link">https://relink/k4lKyk</a>
-          <button class="copy">
-           Copy
-          </button>
+      `<li class="shortened-li">
+        <div class="shortened-div">
+         <p class="original">${urlInput.value}</p>
+         <div>
+           <a class="output">Shortened link goes here!</a>
+           <button class="copy">
+            Copy
+           </button>
+         </div>
         </div>
-       </div>`
+       <li>`
     );
     urlInput.value = "";
   }
 });
 
-const copyContent = async () => {
-  try {
-    await navigator.clipboard.writeText(copiedLink.innerHTML);
-    alert("Copied: " + copiedLink.innerHTML);
-    console.log("Content copied to clipboard");
-  } catch (err) {
-    console.error("Failed to copy: ", err);
-    alert("Oops, something went wrong.");
-  }
-};
-
-copyButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    document.querySelector(".copied")?.classList.remove("copied");
-    event.currentTarget.textContent = "Copied!";
-    event.currentTarget.classList.add("copied");
+shortenedLinks.addEventListener("click", (event) => {
+  if (event.target.className === "copy") {
+    copiedButton = document.querySelector(".copied");
+    copiedButton?.classList.remove("copied");
+    event.target.textContent = "Copied!";
+    event.target.classList.add("copied");
     navigator.clipboard.writeText(
-      event.currentTarget.previousElementSibling.textContent
+      event.target.previousElementSibling.textContent
     );
-  });
+  }
 });
