7b10c815c9ff8e041218bc92f4c52508c74a6136
Ronald Sayre III <115036926+RJ3605@users.noreply.github.com>
2022-12-19 10:29:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*import FetchWrapper from "./fetch-wrapper.js";*/
+import FetchWrapper from "./fetch-wrapper.js";
 
 const shortenedLinks = document.querySelector("#shortened-links");
 const shortenedLink = document.querySelector(".shortened-li");
@@ -7,33 +7,40 @@ const urlInput = document.querySelector("#url-input");
 const copy = document.querySelector(".copy");
 const copyButtons = document.querySelectorAll(".copy");
 const output = document.querySelector(".output");
-/*const API = new FetchWrapper('')*/
+const API = new FetchWrapper("https://api.shrtco.de/v2");
 
 /*This places the html segment that contains the original and shortened links*/
-shortenIt.addEventListener("click", async () => {
+shortenIt.addEventListener("click", async (event) => {
+  event.preventDefault();
   if (urlInput.value !== "") {
-    /*submit posts to API
-    await API get link*/
-    shortenedLinks.insertAdjacentHTML(
-      `beforeend`,
-      `<li class="shortened-li">
+    await API.get(`/shorten?url=${urlInput.value}`, {
+      url: urlInput.value,
+    }).then((data) => {
+      console.log(data);
+      shortenedLinks.insertAdjacentHTML(
+        `beforeend`,
+        `<li class="shortened-li">
         <div class="shortened-div">
          <p class="original">${urlInput.value}</p>
          <div>
-           <a class="output">Shortened link goes here!</a>
+           <a class="output">${data.result.short_link}</a>
            <button class="copy">
             Copy
            </button>
          </div>
         </div>
        <li>`
-    );
-    urlInput.value = "";
+      );
+      urlInput.value = "";
+    });
   }
 });
 
 shortenedLinks.addEventListener("click", (event) => {
   if (event.target.className === "copy") {
+    document.querySelectorAll(".copy")?.forEach((button) => {
+      button.textContent = "Copy";
+    });
     document.querySelector(".copied")?.classList.remove("copied");
     event.target.textContent = "Copied!";
     event.target.classList.add("copied");
